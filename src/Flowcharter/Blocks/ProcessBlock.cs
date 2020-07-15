4698ea295be24588203d29581b5e268838444333
Alex <alexander.g@btigroup.io>
2020-07-16 01:16:21 +0300

    Create extension for ProcessBlock

--- src/Flowcharter/Blocks/ProcessBlock.cs
+++ src/Flowcharter/Blocks/ProcessBlock.cs
@@ -2,22 +2,43 @@
 
 namespace Flowcharter.Blocks
 {
-    public class ProcessBlock<TInput, TOutput> : IReceiverBlock<TInput>
+    public class ProcessBlock<TInput, TOutput> : IReceiverBlock<TInput>, IProducerBlock<TOutput>
     {
         private readonly Func<TInput, PipelineMetadata, TOutput> _process;
-        private readonly IReceiverBlock<TOutput> _next;
+        private IReceiverBlock<TOutput> _following;
 
-        public ProcessBlock(Func<TInput, PipelineMetadata, TOutput> process, IReceiverBlock<TOutput> next)
+        public ProcessBlock(Func<TInput, PipelineMetadata, TOutput> process) : this(process, null)
+        {
+        }
+
+        public ProcessBlock(Func<TInput, PipelineMetadata, TOutput> process, IReceiverBlock<TOutput> receiverBlock)
         {
             _process = process;
-            _next = next;
+            _following = receiverBlock;
+        }
+
+        public void Link(IReceiverBlock<TOutput> receiverBlock)
+        {
+            _following = receiverBlock;
         }
 
         public void Process(TInput input, PipelineMetadata metadata)
         {
             var output = _process(input, metadata);
 
-            _next?.Process(output, metadata);
+            _following?.Process(output, metadata);
+        }
+    }
+
+    public static class ProcessBlockExtensions
+    {
+        public static ProcessBlock<TPrecedingOutput, TCurrentOutput> Process<TPrecedingOutput, TCurrentOutput>(
+            this IProducerBlock<TPrecedingOutput> precedingBlock,
+            Func<TPrecedingOutput, PipelineMetadata, TCurrentOutput> process)
+        {
+            var next = new ProcessBlock<TPrecedingOutput, TCurrentOutput>(process);
+            precedingBlock.Link(next);
+            return next;
         }
     }
 }
--- src/Flowcharter/IReceiverBlock.cs
+++ src/Flowcharter/IReceiverBlock.cs
@@ -1,7 +1,14 @@
-﻿namespace Flowcharter
+﻿using Flowcharter.Blocks;
+
+namespace Flowcharter
 {
     public interface IReceiverBlock<in TInput>
     {
         void Process(TInput input, PipelineMetadata metadata);
+    }
+    
+    public interface IProducerBlock<out TOutput>
+    {
+        void Link(IReceiverBlock<TOutput> receiverBlock);
     }
 }
